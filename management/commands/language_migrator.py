1fbb8a06d9fc38e9e4009b3b6c318678350b2093
Lapo Marchionni <marchionni.lapo@gmail.com>
2016-09-02 11:20:38 +0200

    Add files via upload

--- management/commands/language_migrator.py
+++ management/commands/language_migrator.py
@@ -3,7 +3,9 @@ from django.utils import translation
 from django.utils.translation import ugettext
 from sys import stdout
 import os, os.path
-import re
+import re, sys
+import py_compile
+from distutils import dir_util
 
 class Command(BaseCommand):
 
@@ -11,6 +13,16 @@ class Command(BaseCommand):
 	help = 'Translate all html and py files in your project to a selected language from a po file'
 
 	def handle(self, *args, **options):
+
+		def query_yes_no(question):
+			valid = {"yes": True, "y": True, "ye": True, "no": False, "n": False}
+			while True:
+				sys.stdout.write(question)
+				choice = raw_input().lower()
+				if choice in valid:
+					return valid[choice]
+				else:
+					return False
 
 		def get_files():
 			counter_html, counter_py = 0, 0
@@ -50,6 +62,8 @@ class Command(BaseCommand):
 					f.seek(0)
 					f.write(text.encode('utf-8'))
 					f.truncate()
+				if file.endswith('.py'):
+					py_compile.compile(path, doraise=True)
 
 		try:
 			lang = args[0]
@@ -59,7 +73,12 @@ class Command(BaseCommand):
 			return
 
 		counter_html, counter_py, directories = get_files()
-		print 'Found %s html files and %s py files across %s directories' % (counter_html, counter_py, len(directories))
+		print 'Found %s files across %s directories [%s .html and %s .py]\n' % (counter_html + counter_py, len(directories), counter_html, counter_py)
+		if query_yes_no('These files will be modified, are you sure? ') == False:
+			return
+
+		print 'Backupping project folder...'
+		dir_util.copy_tree('.', './mig_bkup_dta')
 
 		error_files = []
 		dir_count = 0
@@ -71,7 +90,7 @@ class Command(BaseCommand):
 				try:
 					translate_file(path)
 				except Exception as e:
-					error_files.append(path + ' Error: ' + str(e))
+					error_files.append(path)
 				file_count += 1
 				stdout.write("\r[%s of %s]\tTranslated %s of %s \t\t%s" % (dir_count, len(directories), file_count, len(directories[root]), root))
 				stdout.flush()
@@ -79,3 +98,15 @@ class Command(BaseCommand):
 
 		if len(error_files) != 0:
 			print 'There were %s errors with these files: \n%s' %(len(error_files), '\n'.join(error_files))
+			print 'Reverting this files from backup...'
+			for file_path in error_files:
+				print file_path
+
+		if query_yes_no('Translation done, do you want to revert to the backup data? '):
+			dir_util.copy_tree('./mig_bkup_dta', '.')
+			print 'Reverted to original state'
+		if query_yes_no('Do you want to keep the backup? '):
+			dir_util.remove_tree('./mig_bkup_dta')
+		else:
+			print 'Backup saved in folder /mig_bkup_dta in your project folder'
+		print 'Done'
